a38b85caa027dc10753831e4b5ae7d5a01dabec6
M Sah <msahin@comu.edu.tr>
2022-08-09 15:17:04 +0300

    i added log4j

--- src/test/java/stepdefinitions/SampleScenarioStepDefs.java
+++ src/test/java/stepdefinitions/SampleScenarioStepDefs.java
@@ -6,7 +6,8 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import pages.GooglePage;
 import utilities.Driver;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class SampleScenarioStepDefs {
 /*
 @ui @samplescenario
@@ -21,6 +22,8 @@ Feature: This is sample feature
       | webpage                |
       | https://www.google.com |
  */
+
+    private static Logger logger =LogManager.getLogger(SampleScenarioStepDefs.class.getName());
     private GooglePage googlePage;
 
     public SampleScenarioStepDefs(GooglePage googlePage) {
@@ -29,18 +32,25 @@ Feature: This is sample feature
 
     @Given("As a user go to the {string} page")
     public void as_a_user_go_to_the_page(String webpage) {
+
         Driver.getDriver().get(webpage);
+        logger.info(webpage+" page was opened");
     }
     @Then("As a user verify the page title is not empty")
     public void as_a_user_verify_the_page_title_is_not_empty() {
         Assert.assertFalse(Driver.getDriver().getTitle().isEmpty());
+        logger.info("Page title is not empty");
     }
     @Then("As a user search {string} on google page")
     public void as_a_user_search_on_google_page(String searchWord) {
+
         googlePage.search(searchWord);
+        logger.info(searchWord+" was searched");
     }
     @Then("As a user close the web page")
     public void as_a_user_close_the_web_page() {
+
         Driver.closeDriver();
+        logger.info("driver was closed");
     }
 }
